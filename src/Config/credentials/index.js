2e315848e4655208506d13dcaebbc65041a24389
GarutCodeCamp <akuncode391@gmail.com>
2021-07-20 23:24:51 +0700

    break

--- src/Config/credentials/index.js
+++ src/Config/credentials/index.js
@@ -4,9 +4,21 @@ const client_and_secret = {
 }
 
 
-const url_Authorize = `https://accounts.spotify.com/authorize?client_id=${client_and_secret.client_Id}&response_type=code&redirect_uri=http://localhost:3000/&state=STATE&scope=user-read-private%20user-read-email&show_dialog=true`;
+const url_Authorize = `https://accounts.spotify.com/authorize?client_id=${client_and_secret.client_Id}&response_type=token&redirect_uri=http://localhost:3000/&state=STATE&scope=user-read-private%20user-read-email&show_dialog=true`;
 
+const getToken_Url=()=>{
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial,item)=>{
+        let parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+    })
 
+}
 export {
-    url_Authorize
+    url_Authorize,
+    client_and_secret,
+    getToken_Url
 }
--- src/Pages/spotify/index.jsx
+++ src/Pages/spotify/index.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 import Button from '../../Components/Button';
-import { url_Authorize } from '../../Config/credentials';
+import { url_Authorize,client_and_secret,getToken_Url } from '../../Config/credentials';
 import "./index.css"
+
 export default class LoginSpotify extends Component {
-    
+    constructor(props){
+        super(props);
+        this.state={
+            token: "",
+
+            
+        }
+    }
+    componentDidMount=()=>{
+        
+        const tokens = getToken_Url();
+        console.log("ini token"+tokens);
+    }
     render() {
         return (
             <div className="bg-spotify">
