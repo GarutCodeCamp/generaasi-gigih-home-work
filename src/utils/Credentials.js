d5ea52a577bc4d7dcdaa6b4c8e658317f5377ad0
GarutCodeCamp <akuncode391@gmail.com>
2021-08-15 21:36:12 +0700

    test redirect

--- src/utils/Credentials.js
+++ src/utils/Credentials.js
@@ -1,16 +1,16 @@
 const CLIENT_ID = process.env.REACT_APP_CLIENTID;
-const SPOTIFY_AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize';
-const SCOPE = ['playlist-modify-private', 'user-read-email'];
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const SCOPE = ["playlist-modify-private", "user-read-email"];
 
 const loginSpotify = () => {
-  window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000/&scope=${SCOPE}&response_type=token&show_dialog=true`;
+  window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=https://6119249bf3aafe96b4e1438d--ahmadjun-gg063.netlify.app/&scope=${SCOPE}&response_type=token&show_dialog=true`;
 };
 
 const getAccessToken = (hash) => {
   const stringAfterHastag = hash.substring(1);
-  const paramInUrl = stringAfterHastag.split('&');
+  const paramInUrl = stringAfterHastag.split("&");
   const paramSplitUp = paramInUrl.reduce((acc, currentValue) => {
-    const [key, value] = currentValue.split('=');
+    const [key, value] = currentValue.split("=");
     acc[key] = value;
     return acc;
   }, {});
